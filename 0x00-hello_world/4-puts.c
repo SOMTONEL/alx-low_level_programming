2d3339c6026fc6d68426d7739c6a7f0c078ccd89
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-08 19:48:12 -0800

    Programming defs

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: 'prints text'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
