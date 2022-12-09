8ea7a4449343462762f1a6bdb0eea51e369bfad3
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-09 14:51:26 -0800

    comt

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints some text'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char Le;
+
+	/* your code goes there */
+	for (Le = 'a'; Le <= 'z'; Le++)
+	{
+		putchar(Le);
+	}
+	putchar('\n');
+	return (0);
+}
