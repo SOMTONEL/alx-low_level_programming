137a52088e88e22472af93fe7aca337ed2b60e60
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-10 06:19:47 -0800

    comt

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints numbers'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int Le = '0';
+	int Lt = '0';
+
+	/* your code goes there */
+	for (Lt = '0'; Lt <= '9'; Le++)
+	{
+		for (Le = '0'; Le <= '9'; Le++)
+		{
+			if (!((Le == Lt) || (Lt > Le)))
+			{
+				putchar(Lt);
+				putchar(Le);
+				if (!(Le == '9' && Lt == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
