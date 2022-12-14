a26bf8560d45e4c6886e41b60d247f77b3d6eca3
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-13 16:55:57 -0800

    comt

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - 'prints last digit of a number'
+ *
+ * @n: integer
+ *
+ * Return: 0 if n is 0
+ */
+
+int print_last_digit(int n)
+{
+	int r = n % 10;
+
+	return (r);
+}
