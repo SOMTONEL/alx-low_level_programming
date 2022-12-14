e9e0e6ef2d3af21c55c9ce41a87a63d256aa717c
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-13 19:17:31 -0800

    comt

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - 'adds two integers'
+ *
+ * @n: integer
+ *
+ * @b: integer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int n, int b)
+{
+	_putchar(n + b);
+	return (0);
+}
+
