e05779e34ba9c831403cc57c0048753d4af44d06
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-14 16:49:56 -0800

    comt

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - 'prints times table of a number'
+ *
+ *@n: integer
+ *
+ * Return: 0 if n is 0
+ */
+
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('0');
+		for (j = 1; j <= n; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			k = j * i;
+			if (k < 10)
+				_putchar(' ');
+			else
+				_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
