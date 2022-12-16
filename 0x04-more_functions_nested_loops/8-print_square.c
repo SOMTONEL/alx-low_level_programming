3faeaa262faf06b83d405c586ad28e636e49a6f3
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-15 20:36:58 -0800

    comt

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints square
+ *
+ * @size: integer
+ * Return: nothing to return
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		if (size <= 0)
+			_putchar('\n');
+		else
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+		}
+	}
+	_putchar('\n');
+}
