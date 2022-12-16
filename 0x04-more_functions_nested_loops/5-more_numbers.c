3f5d6fe5ad3c7dfaed2f8e91f4cef2ad8c4e41f7
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-15 19:48:55 -0800

    comt

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,14 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = '0'; j < 15; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
