a002f374c236a3fdf6987088df0f26cc39d36f7e
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-15 19:11:10 -0800

    comt

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers
+ *
+ * Return: nothing to return
+ */
+
+void print_most_numbers(void)
+{
+	char i = '0';
+
+	if (i != '2' && i != '4')
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
