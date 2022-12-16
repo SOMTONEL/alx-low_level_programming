2b5f5068f52937fb177eb95a5462bb3e3499243a
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-15 19:58:43 -0800

    comt

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ *
+ * @n: integer
+ *
+ * Return: nothing to return
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+			_putchar('\n');
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
