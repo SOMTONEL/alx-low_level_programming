5e71cb24b9a9d63681268006b8b986ff74e328b7
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-15 18:53:12 -0800

    comt

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,7 +6,7 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-int convert_day(int month, int day);
-void print_times_table(int n);
+int _putchar(char c);
+void print_numbers(void);
 
 #endif /* MAIN_H */
