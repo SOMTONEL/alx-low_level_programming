0aa46c5be182ab6143be0fb6baf62ac0770ad88a
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-15 18:32:38 -0800

    comt

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - 'checks for digits within range of 0 - 9'
+ *
+ * @c: integer
+ *
+ * Return: 1 or 0 depending on condition
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int _isupper(int c);
-int largest_number(int a, int b, int c);
+int _isdigit(int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 void print_times_table(int n);
