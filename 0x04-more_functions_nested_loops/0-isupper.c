1cd9422f3a5e9dd4cb56035b4175a9d19474708f
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-15 18:01:47 -0800

    comt

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - 'checks for uppercase characters'
+ *
+ * @c: character
+ *
+ * Return: 1 if upper, 2 if lower
+ */
+
+int _isupper(int c)
+{
+	if (c == 'A')
+		return (1);
+	else
+		return (0);
+}
