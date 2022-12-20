fa37cb783103edf75b454a7d4622fb4af25a3f45
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-20 13:40:00 -0800

    updates to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value
+ *
+ * @*n: a pointer
+ *
+ * @n: an integer
+ *
+ * Return: nothing to return.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
