4a4cc53c8c03700c0e1f3c0f563df4073455b696
SOMTONEL <benji.chukwu.20@gmail.com>
2022-12-20 14:04:20 -0800

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ *
+ * @s: a character string
+ * @*s: a pointer to character string
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int L;
+
+	L = sizeof(*s);
+	return (L);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int mul(int a, int b);
+int _strlen(char *s);
 int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
