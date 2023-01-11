ee383b78d381ac48fdea629070f2f69e16d39558
SOMTONEL <benji.chukwu.20@gmail.com>
2023-01-10 18:49:20 -0800

    alloc_grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2D array of ints
+ * @width: an integer representing distance
+ * @height: an integer representing distance
+ * Return: NULL on failure
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, k, l;
+	int **ptr;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		ptr = (int **) malloc(height * sizeof(int *));
+
+		if (!ptr)
+		{
+			free(ptr);
+			return (NULL);
+		}
+		for (i = 0; i < height; i++)
+		{
+			ptr[i] = (int *) malloc(width * sizeof(int));
+			if (!ptr[i])
+			{
+				for (j = 0; j <= i; j++)
+					free(ptr);
+				free(ptr);
+				return (NULL);
+			}
+		}
+		for (k = 0; k < height; k++)
+		{
+			for (l = 0; l < width; l++)
+			{
+				ptr[k][l] = 0;
+			}
+		}
+		return (ptr);
+	}
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,7 +5,7 @@ char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-int _isalpha(int c);
+int **alloc_grid(int width, int height);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
