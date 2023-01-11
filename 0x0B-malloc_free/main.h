bc58118a3a5aec3770dee6f2b5cde272bc9ae99a
SOMTONEL <benji.chukwu.20@gmail.com>
2023-01-10 19:05:41 -0800

    DEALLOCATION

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,13 +6,7 @@ int _putchar(char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
-int print_sign(int n);
+void free_grid(int **grid, int height);
 int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
 
 #endif /**MAIN_H**/
