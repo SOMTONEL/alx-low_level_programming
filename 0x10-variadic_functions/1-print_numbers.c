7691a5db6d86bfbdbf2f19e7294fdb863e8db6a8
SOMTONEL <benji.chukwu.20@gmail.com>
2023-01-22 17:12:22 -0800

    To be is to be

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ *
+ * @separator: a char pointer to a string literal ','
+ * @n: number of arguments/numbers to be printed
+ *
+ * Return: nothing but take not of the if
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+			break;
+		printf("%d%s", va_arg(ap, int), separator);
+	}
+	printf("\n");
+
+	va_end(ap);
+}
