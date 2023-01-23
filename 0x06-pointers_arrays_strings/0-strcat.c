64ab489fad161f23b064379e8a7d92992803a598
SOMTONEL <benji.chukwu.20@gmail.com>
2023-01-23 09:31:45 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings by returning a pointer to a string
+ * @dest: pointer to concatenated string
+ * @src: pointer to a string
+ * Return: pointer to concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	unsigned int i = 0, j = 0;
+	char *str = (char *)malloc(100);
+
+	while (dest[i] != '\0')
+	{
+		str[j] = dest[i];
+		i++;
+		j++;
+	}
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		str[j] = src[i];
+		i++;
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,8 @@
+0x06.C - Pointers, Arrays, and Strings
+This project sees a deeper dive into the concepts of pointers, arrays, and character arrays, which are strings.
+
+int *
+int **
+int ***
+
+Then again this is just another readme file.
